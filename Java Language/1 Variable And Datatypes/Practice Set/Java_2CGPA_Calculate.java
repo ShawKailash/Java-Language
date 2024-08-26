bbf40b4c44b04acf194228911f42f47b83eb06d7
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-26 19:28:28 +0530

    Add files via upload

---
+++ Java Language/1 Variable And Datatypes/Practice Set/Java_2CGPA_Calculate.java
@@ -0,0 +1,11 @@
+// write a program to calculate CGPA using marks of three subject (out of 100)
+
+public class Java_2CGPA_Calculate {
+    public static void main(String[] args) {
+        float a=56;
+        float b=78;
+        float c=89;
+        float CGPA=(a+b+c)/30;
+        System.out.println(CGPA);
+    }
+}
