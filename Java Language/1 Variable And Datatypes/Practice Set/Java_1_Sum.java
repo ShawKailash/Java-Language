aeccb27bf71f26bd2366a81043c134dadc3cc5dd
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-26 19:27:51 +0530

    Add files via upload

---
+++ Java Language/1 Variable And Datatypes/Practice Set/Java_1_Sum.java
@@ -0,0 +1,11 @@
+// write a program to sum three number in java
+
+public class Java_1_Sum {
+    public static void main (String args[]){
+        int a=3;
+        int b=6;
+        int c=7;
+        int sum=a+b+c;
+        System.out.println(sum);
+    }
+}
