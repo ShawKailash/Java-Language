5e50f61ff4669522e8d104f97377705667fdbd3f
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-27 10:55:21 +0530

    Add files via upload

---
+++ Java Language/1 Variable And Datatypes/Practice Set/Java_5_Number.java
@@ -0,0 +1,10 @@
+// write a java program to detect whether a number entered
+// by the user is integer or not
+
+import java.util.Scanner;
+public class Java_5_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
