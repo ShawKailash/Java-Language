91dbba39478b67ba72edceebdadd305d83036cf9
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-28 17:10:54 +0530

    Add files via upload

---
+++ Java Language/1 Variable And Datatypes/Java_3_Variable_Datatype.java
@@ -0,0 +1,11 @@
+public class Java_3_Variable_Datatype {
+    public static void main (String args[]){
+        int num1=3;
+        int num2=6;
+        int num3=5;
+        int sum=num1+num2+num3;
+        System.out.println("the sum is ");
+        System.out.println(sum);
+    }
+    
+}
