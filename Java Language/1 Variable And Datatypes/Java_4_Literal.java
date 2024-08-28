1143edadc03e2be35aef354f7933a1c97ac23512
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-28 17:11:23 +0530

    Add files via upload

---
+++ Java Language/1 Variable And Datatypes/Java_4_Literal.java
@@ -0,0 +1,21 @@
+public class Java_4_Literal {
+    public static void main(String[] args) {
+        int age=34;
+        byte age1=56;
+        short age2=78;
+        long lo=2342523525443554l;
+        char ch='a';
+        float f1=34.5667f;
+        double d1=34.678d;
+        boolean b=true;
+        System.out.println(age);
+        System.out.println(age1);
+        System.out.println(age2);
+        System.out.println(lo);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(b);
+    }
+    
+}
