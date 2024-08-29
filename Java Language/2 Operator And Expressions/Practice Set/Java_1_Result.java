2c3536f3275990dea8318956e25317c257d0e6c3
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-29 10:47:43 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Practice Set/Java_1_Result.java
@@ -0,0 +1,9 @@
+// what will be the result of the following expression
+// float a=7/4*9/2
+
+public class Java_1_Result {
+    public static void main(String[] args) {
+        float a=7/4.0f*9/2.0f;
+        System.out.println(a);
+    }
+}
