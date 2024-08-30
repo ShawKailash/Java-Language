2092f60599519065f438daefe7bb22fe0b38fc4c
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-30 16:12:52 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Practice Set/Java_3_Greater_Than.java
@@ -0,0 +1,11 @@
+// use comparison operators to find out whether a given number
+// is greater than the user entered number or not
+
+import java.util.Scanner;
+public class Java_3_Greater_Than {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);
+    }
+}
