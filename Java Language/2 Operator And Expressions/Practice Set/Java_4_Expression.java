10a7c1380179c85b513a214b947c6442d898ff32
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-30 16:13:28 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Practice Set/Java_4_Expression.java
@@ -0,0 +1,12 @@
+// write the following expressionin a java program (v*v-u*u)/(2*a*s)
+
+public class Java_4_Expression {
+    public static void main(String[] args) {
+        float v=4;
+        float u=3;
+        float a=2;
+        float s=1;
+        float cal=(v*v-u*u)/(2*a*s);
+        System.out.println(cal);
+    }
+}
