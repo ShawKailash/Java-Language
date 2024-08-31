a12fac6b88cb8267359a4fedfa1698ce348513e6
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-31 11:37:49 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Java_8_Type_Of_Operator.java
@@ -0,0 +1,14 @@
+public class Java_8_Type_Of_Operator {
+    public static void main(String[] args) {
+        int a=10;
+        int b=6;
+        int sum = a+b;
+        System.out.println(sum);
+        int c=7;
+        c*=5;
+        System.out.println(c);
+        System.out.println(64<5);
+        System.out.println(64>5);
+        System.out.println(64>5 && 64<34);
+    }
+}
