07a68f93a9cacc5cb3ea50f52548e744849accdd
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-31 11:39:14 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Java_9_Quick_Quiz.java
@@ -0,0 +1,22 @@
+// quick quiz 
+// how will you write the following expression in java
+public class Java_9_Quick_Quiz {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int c=4;
+        int d=5;
+        int u=6;
+        int v=7;
+        int x=8;
+        int y=9;
+        int cal1=x-y/2;
+        int cal2=(b*b-4*a*c)/(2*a);
+        int cal3=v*v-u*u;
+        int cal4=a*b-d;
+        System.out.println(cal1);
+        System.out.println(cal2);
+        System.out.println(cal3);
+        System.out.println(cal4);
+    }
+}
