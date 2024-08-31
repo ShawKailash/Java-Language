03bc26d1c449590aec4d2ecf55a7ed208e636e6a
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-31 11:38:36 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Java_9_Precedence_Associativity.java
@@ -0,0 +1,8 @@
+public class Java_9_Precedence_Associativity {
+    public static void main(String[] args){
+        int a=6*5-6/2;
+        int b=60/5-4*2;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
