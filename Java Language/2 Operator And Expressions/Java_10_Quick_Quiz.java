9f1555cc6bcee322bb72a7bbb9a5acc27be37c93
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-01 12:49:00 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Java_10_Quick_Quiz.java
@@ -0,0 +1,15 @@
+// quick quiz
+// what will be the value of the following expression(x)
+// int y=y;
+// int x=++y*8;
+// value of x ?
+
+public class Java_10_Quick_Quiz {
+    public static void main(String[] args) {
+        int y=7;
+        int x=++y*8;
+        System.out.println(x);
+        char ch='b';
+        System.out.println(++ch);
+    }
+}
