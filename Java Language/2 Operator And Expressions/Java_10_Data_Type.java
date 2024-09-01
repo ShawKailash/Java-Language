383301d3f7054341ee0ec0980b9fe9258a0c6024
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-01 12:48:24 +0530

    Add files via upload

---
+++ Java Language/2 Operator And Expressions/Java_10_Data_Type.java
@@ -0,0 +1,16 @@
+public class Java_10_Data_Type {
+    public static void main(String[] args) {
+        int x=4;
+        int y=8;
+        int a=x+y;
+        float b=45.34f+x;
+        System.out.println(a);
+        System.out.println(b);
+        int i=45;
+        System.out.println(i++);
+        System.out.println(i);
+        int j=56;
+        System.out.println(j);
+        System.out.println(++j);
+    }
+}
