96d35908fbe5211fcc0aaf3705f5a4190df91200
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-02 20:36:55 +0530

    Add files via upload

---
+++ Java Language/3 String/Practice Set/Java_2_Replace.java
@@ -0,0 +1,9 @@
+// write a java program to replace spaces with underscores
+
+public class Java_2_Replace {
+    public static void main(String[] args) {
+        String text = "my name is kailash";
+        text = text.replace(" ", "_");
+        System.out.println(text);
+    }    
+}
