7a2a3b5ebf7cab6ee94f82791ee7eb2a759e04a4
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-02 20:36:07 +0530

    Add files via upload

---
+++ Java Language/3 String/Practice Set/Java_1_Lower_Case.java
@@ -0,0 +1,8 @@
+// write a java program to convert string to lower case
+public class Java_1_Lower_Case {
+    public static void main(String[] args) {
+        String name = "kAILASH";
+        name = name.toLowerCase();
+        System.out.println(name);
+    }
+}
