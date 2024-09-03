e31bf70c7e12e4a3505ef0ba304bdfe607fc3d82
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-03 12:33:35 +0530

    Add files via upload

---
+++ Java Language/3 String/Practice Set/Java_5_Letter_Format.java
@@ -0,0 +1,9 @@
+// write a java program to format the following letter using escape sequence characters
+// letter = "dear kailash, this java course is nice. thanks"
+
+public class Java_5_Letter_Format {
+    public static void main(String[] args) {
+        String letter = "dear kailash,\n\tthis java course is nice.\nthanks";
+        System.out.println(letter);
+    }
+}
