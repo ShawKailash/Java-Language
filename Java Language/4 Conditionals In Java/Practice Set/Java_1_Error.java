13dc1d79984c3e4daf37137bf6d80f1e4f6e2d5d
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-04 20:42:13 +0530

    Add files via upload

---
+++ Java Language/4 Conditionals In Java/Practice Set/Java_1_Error.java
@@ -0,0 +1,24 @@
+// what will be the output of this program 
+// int a=10;
+// if(a==11)
+// {
+//     System.out.println("i am 11");
+// }
+// else
+// {
+//     System.out.println("i am not 11")
+// }
+
+public class Java_1_Error {
+    public static void main(String[] args) {
+        int a=10;
+        if(a==11)
+        {
+            System.out.println("i am 11");
+        }
+        else
+        {
+            System.out.println("i am not 11");
+        }
+    }
+}
