3926de2052661cb37597693e75b3ebc5982f1c0f
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-06 10:31:20 +0530

    Add files via upload

---
+++ Java Language/4 Conditionals In Java/Java_16_If_Else.java
@@ -0,0 +1,13 @@
+public class Java_16_If_Else {
+    public static void main(String[] args) {
+        int age = 18;
+        if(age>=18)
+        {
+            System.out.println("yes boy you can drive!");
+        }
+        else
+        {
+            System.out.println("no boy you cannot drive yet!");
+        }
+    }
+}
