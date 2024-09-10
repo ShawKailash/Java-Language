67defe66c7ac9fb0d7d3da22785452d1e457f7ad
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-10 11:03:55 +0530

    Add files via upload

---
+++ Java Language/5 Loop Control Instruction/Java_21_2_Quick_Quiz.java
@@ -0,0 +1,14 @@
+// quick quiz
+// write a java program to print natural numbers from 100 to 200
+
+public class Java_21_2_Quick_Quiz {
+    public static void main(String[] args) {
+        System.out.println("print the number 100 to 200");
+        int i = 100;
+        while(i<=200)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
