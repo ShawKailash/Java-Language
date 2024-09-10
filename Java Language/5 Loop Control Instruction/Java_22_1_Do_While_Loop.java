46aaeb5f3a79e9c8d64a31e3e4147f46d09b2598
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-10 11:04:30 +0530

    Add files via upload

---
+++ Java Language/5 Loop Control Instruction/Java_22_1_Do_While_Loop.java
@@ -0,0 +1,12 @@
+public class Java_22_1_Do_While_Loop {
+    public static void main(String[] args) {
+        System.out.println("using do while loop");
+        int i=10;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=40);
+    }
+}
