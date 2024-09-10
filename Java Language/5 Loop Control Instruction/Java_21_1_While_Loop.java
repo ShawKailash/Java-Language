cb8b66641d8b48583c753a4cfbe14aa45d605978
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-10 11:03:21 +0530

    Add files via upload

---
+++ Java Language/5 Loop Control Instruction/Java_21_1_While_Loop.java
@@ -0,0 +1,14 @@
+public class Java_21_1_While_Loop {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("using while loop");
+        int i=1;
+        while(i<=3)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
