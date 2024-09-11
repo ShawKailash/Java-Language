91363ae1f2e1e70c402637c27985c40f9f12673e
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-11 20:15:02 +0530

    Add files via upload

---
+++ Java Language/5 Loop Control Instruction/Java_23_1_For_Loop.java
@@ -0,0 +1,9 @@
+public class Java_23_1_For_Loop {
+    public static void main(String[] args) {
+        System.out.println("using for loop");
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
