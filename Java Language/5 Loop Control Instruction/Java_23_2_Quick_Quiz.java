82ef1f652ef4322474dfa905d5ef6af37f997121
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-11 20:15:48 +0530

    Add files via upload

---
+++ Java Language/5 Loop Control Instruction/Java_23_2_Quick_Quiz.java
@@ -0,0 +1,11 @@
+// quick quiz
+// write a java program to print first n odd number using a for loop
+
+public class Java_23_2_Quick_Quiz {
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i++)
+        {
+            System.out.println(2*i+1);
+        }
+    }
+}
