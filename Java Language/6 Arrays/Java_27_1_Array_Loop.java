66f46c424aab5c6ffa8465cc4ce2828619dd9de1
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-15 13:01:20 +0530

    Add files via upload

---
+++ Java Language/6 Arrays/Java_27_1_Array_Loop.java
@@ -0,0 +1,9 @@
+public class Java_27_1_Array_Loop {
+    public static void main(String[] args) {
+        int [] marks = {56,78,34,89,75,63,53};
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+}
