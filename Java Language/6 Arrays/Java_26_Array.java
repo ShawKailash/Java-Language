5edd4a3e7b91ea33c5987f02e213bab397e38cc9
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-15 13:00:46 +0530

    Add files via upload

---
+++ Java Language/6 Arrays/Java_26_Array.java
@@ -0,0 +1,26 @@
+public class Java_26_Array {
+    public static void main(String[] args) {
+        int [] m1 = new int[5];
+        m1[0]=45;
+        m1[1]=89;
+        m1[2]=56;
+        m1[3]=78;
+        m1[4]=67;
+        System.out.println(m1[3]);
+        int [] m2;
+        m2 = new int[10];
+        m2[0]=46;
+        m2[1]=57;
+        m2[2]=68;
+        m2[3]=76;
+        m2[4]=87;
+        m2[5]=83;
+        m2[6]=74;
+        m2[7]=65;
+        m2[8]=59;
+        m2[9]=88;
+        System.out.println(m2[6]);
+        int [] m3 = {60,70,50,80,90,40,55,66,77,44};
+        System.out.println(m3[5]);
+    }
+}
