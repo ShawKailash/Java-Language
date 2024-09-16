e02276d34f8dc008192b535e61d66a3cabad8230
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-09-16 19:34:45 +0530

    Add files via upload

---
+++ Java Language/6 Arrays/Java_27_3_Array_For_Each_Loop.java
@@ -0,0 +1,9 @@
+public class Java_27_3_Array_For_Each_Loop {
+    public static void main(String[] args) {
+        int [] marks = {34,45,56,67,78,89,94};
+        for(int element: marks)
+        {
+            System.out.println(element);
+        }
+    }
+}
