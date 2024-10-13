693c6e92edb018ea37404ddd497dc9e6f9aad0bc
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-10-13 21:17:32 +0530

    Add files via upload

---
+++ Java Language/13 Multithreading/Practice Set/Java_5_Reference_Current_Thread.java
@@ -0,0 +1,7 @@
+// how do you get reference to the current thread in java
+
+public class Java_5_Reference_Current_Thread {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getState());
+    }
+}
