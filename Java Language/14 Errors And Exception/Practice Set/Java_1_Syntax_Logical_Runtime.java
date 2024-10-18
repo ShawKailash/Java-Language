8948f126a3f6c2724115abe5aa79afaca88c4e4e
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-10-18 14:59:20 +0530

    Add files via upload

---
+++ Java Language/14 Errors And Exception/Practice Set/Java_1_Syntax_Logical_Runtime.java
@@ -0,0 +1,15 @@
+// write a java program to demonstrate syntax logical and runtime errors
+
+public class Java_1_Syntax_Logical_Runtime {
+    public static void main(String[] args) {
+        // syntax
+        int a=9
+
+        // logical
+        int age=78;
+        int yearborn=2000-78;
+
+        // runtime 
+        System.out.println(6/0);
+    }
+}
