f6df63cdf10f86747d0b0e9e2f3a7c2ea3ffcbb7
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-10-25 19:03:11 +0530

    Add files via upload

---
+++ Java Language/15 Advanced Java/Java_96_Data_And_Time.java
@@ -0,0 +1,7 @@
+public class Java_96_Data_And_Time {
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+    }
+}
