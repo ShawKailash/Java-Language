ebdd3dd0a814b1ad31e3e0f5b74ead181dbaf781
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-10-25 19:02:32 +0530

    Add files via upload

---
+++ Java Language/15 Advanced Java/Java_95_Hash_Set.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public interface Java_95_Hash_Set {
+    public static void main(String[] args) {
+        HashSet<Integer> myhashset = new HashSet<>();
+        myhashset.add(6);
+        myhashset.add(8);
+        myhashset.add(3);
+        myhashset.add(9);
+        myhashset.add(8);
+        System.out.println(myhashset);
+    }
+}
